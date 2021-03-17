70bf819dc739468e41a571924770cbf9c3ee8efc
razvanxx11 <dumitru.razvan230796@gmail.com>
2021-03-17 13:34:45 +0200

    [Task-2][razvan]added design on filter and product component. is not the final design

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -2,24 +2,22 @@ import React from 'react';
 
 const Products = ({ products }) => {
   return (
-    <div>
-      <ul className="products-list">
-        {products.map((product) => (
-          <li key={product.id}>
-            <div className="product">
-              <a href={'#' + product.id}>
-                <img src={product.image} alt={product.title} />
-                <p>{product.title}</p>
-              </a>
-              <div className="product-price">
-                <div>{product.price}</div>
-                <button className="button primary">Add to cart</button>
-              </div>
+    <ul className="products-list">
+      {products.map((product) => (
+        <li key={product.id}>
+          <div className="product">
+            <a href={'#' + product.id}>
+              <img src={product.image} alt={product.title} />
+              <p>{product.title}</p>
+            </a>
+            <div className="product-price">
+              <div className="product-price_value">$ {product.price}</div>
+              <button className="button primary">Add to cart</button>
             </div>
-          </li>
-        ))}
-      </ul>
-    </div>
+          </div>
+        </li>
+      ))}
+    </ul>
   );
 };
 
